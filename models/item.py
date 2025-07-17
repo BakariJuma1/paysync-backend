dbf029c1a3a694f37741378143e21ad8b033ba5e
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-07-17 11:24:02 +0300

    Working on models

---
+++ models/item.py
@@ -0,0 +1,13 @@
+from server.extension import db
+from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+
+class Item(db.Model,SerializerMixin):
+    __tablename__ ='items'
+
+    debt_id = db.Column(db.Integer,db.ForeignKey)
+    name = db.Column(db.String,nullable=False)
+    price = db.Column(db.Float,nullable=False)
+    category = db.Column(db.String,nullable=False)
+    quantity = db.Column(db.Integer,nullable=False)
+    total_price = db.C
