9e0d95c11b0ed3c30092b97e924ecf007506db57
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-09-02 12:51:25 +0300

    feat:Adding email column to my customer model and running migrations ,added email to my schema

--- server/models/customer.py
+++ server/models/customer.py
@@ -7,6 +7,7 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_name = db.Column(db.String, nullable=False)
     phone = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
     id_number = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
